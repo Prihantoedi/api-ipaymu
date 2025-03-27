1e01fc67867f84977bf502d6e242f8040f5e7fee
Prihantoedi <prihanto_sanjaya@yahoo.com>
2025-03-27 14:23:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+import cors from 'cors';
+// import bodyParser from 'body-parser';
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import CryptoJS from 'crypto-js';
+// import axios from 'axios';
+import multer from 'multer';
+
+dotenv.config();
+
+const app = express();
+
+// const upload = multer({ dest: 'uploads/' });
+const storage= multer.memoryStorage(); // Use memoryStorage to deployment on Vercel
+const upload = multer({ storage: storage });
+// let allowedOrigins = ['http://localhost'];
+
+// app.use(cors({
+//     origin: function(origin, callback){
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){
+//             let msg = 'The CORS policy for this site does not allow access from the specified origin. ';
+//             return callback(new Error(msg), false);
+//         }
+
+//         return callback(null, true);
+//     }
+// }));
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+const apiKey = process.env.IPAYMU_SANDBOX_API_KEY;
+const va = process.env.IPAYMU_SANDBOX_VA;
+
+const url = 'https://sandbox.ipaymu.com/api/v2/payment';
+
+app.listen(PORT, () => {
+    console.log('Server Listening on PORT: ', PORT);
+});
+
+app.get('/', (request, response) => {
+    const res = {
+        status: 200,
+        message: 'Success'
+    };
+
+    response.status(200).send(res);
+});
+
+app.post('/api/ipaymu/checkout', upload.single('file'), (request, response) => {
+
+    const amount = request.body.amount;
+    const num_of_product = request.body.num_of_product;
+    let body = {
+        'product': ['Macbook'],
+        'qty': [num_of_product],
+        'price' : ['14000000'],
+        'amount' : amount,
+        'returnUrl' : 'http://localhost/doku-payment/success.php',
+        'cancelUrl': 'http://localhost/doku-payment',
+        'notifyUrl' : 'https://gandumbread.com/payment/ipaymu/notification.php',
+        'referenceId' : '086irp',
+        'buyerName' : 'Prihanto',
+        'buyerPhone' : '085219897296',
+        'buyerEmail' : 'prihanto.edys@yahoo.com'
+    };
+
+    let bodyEncrypt = CryptoJS.SHA256(JSON.stringify(body));
+    let stringToSign = 'POST:'+va+':'+bodyEncrypt+':'+apiKey;
+    let signature = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(stringToSign, apiKey));
+
+    fetch(
+        url,
+        {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json', 'Content-Type' : 'application/json',
+                va: va,
+                signature: signature,
+                timestamp: '20150201121045'
+            },
+            body: JSON.stringify(body)
+        }
+    )
+    .then((response) => response.json())
+    .then((responseJson) => {
+        let url = '';
+        // console.log(responseJson);
+        if(responseJson.Status === 200){
+            url = responseJson.Data.Url;
+        }
+        const res = {
+            status: responseJson.Status,
+            success: responseJson.Success,
+            message: responseJson.Message,
+            url_payment: url            
+        };
+    
+        response.status(responseJson.Status).send(res);
+    });
+    
+    
+});
